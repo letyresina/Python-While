5a879aae96aa8e2433470152819446ddf9b8800e
letyresina <leticiaresina@gmail.com>
2023-04-04 09:39:27 -0300

    Subindo exercício 4

---
+++ exercicio4.py
@@ -0,0 +1,17 @@
+'''
+    Exercício 4:
+    Escreva um algoritmo que solicite 10 números e informe quantos números entre 100 e 200 foram 
+    digitados.
+'''
+
+i = 1
+numeros = 0
+
+while i <= 10:
+    num = int(input("Informe um número: "))
+    if (num >= 100 and num <= 200):
+        numeros += 1
+    i += 1
+
+print(f"A quantidade de números digitados entre 100 e 200 foram {numeros}")
+
