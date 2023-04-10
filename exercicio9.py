ece9c906b687d1c8f9f895356fc8872f49cc1bc5
letyresina <leticiaresina@gmail.com>
2023-04-10 20:48:25 -0300

    Iniciado exercício 9, não terminado

---
+++ exercicio9.py
@@ -0,0 +1,14 @@
+'''
+    Exercício 9:
+    Faça um algoritmo que solicite N números e calcule a média dos números pares e a 
+    média dos números ímpares (o valor de N deve ser solicitado ao usuário no início do 
+    programa).
+'''
+
+i = 1;
+i2 = int(input("Informe a quantidade de números que deseja inserir: "))
+
+# Terminar amanhã
+while i <= i2:
+    num = int(input("Digite um número inteiro: "))
+    i += 1
