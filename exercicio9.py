aa135584b6d653e7b1206d7f71fe80fcd7263137
letyresina <leticiaresina@gmail.com>
2023-04-11 08:55:11 -0300

    Feito exercício 9 (confirmar com professor)

--- exercicio9.py
+++ exercicio9.py
@@ -8,7 +8,20 @@
 i = 1;
 i2 = int(input("Informe a quantidade de números que deseja inserir: "))
 
+# Variáveis para pegar pares e ímpares
+pares = 0
+impares = 0
+
 # Terminar amanhã
 while i <= i2:
     num = int(input("Digite um número inteiro: "))
+    if num % 2 == 0: # Vai somar todos os números pares para fazer a média 
+        pares += 1
+        pares / num
+    elif num % 2 != 0:
+        impares += 1
+        impares / num
     i += 1
+
+print(f"A média de números pares é de {pares}")
+print(f"A média de números ímpares é de {impares}")
