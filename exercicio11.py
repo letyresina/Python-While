1b1ff0001917514b539b1cb087e32417a5335e36
letyresina <leticiaresina@gmail.com>
2023-04-11 09:45:15 -0300

    Feito exercício 11, não tem certeza se está certo

--- exercicio11.py
+++ exercicio11.py
@@ -3,3 +3,13 @@
     Escreva um programa que solicita ao usuário o valor de N e calcule o valor de S
     na série presente no PDF em documentacao.
 '''
+
+N = int(input("Informe um valor inteiro qualquer: "))
+i = 1
+resultado = 1
+
+while i <= N:
+    resultado *= 1 / i 
+    i += 1
+
+print(f"O S de {N} é {resultado}")
